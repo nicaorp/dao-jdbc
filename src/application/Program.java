c1ea9418959d4c1e49dccddab64ef85e9a4926e1
Nicolas Ferreira <nicolasferreira100@gmail.com>
2022-06-28 00:18:03 -0300

    Add classe class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Class;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Class obj = new Class(1, "Knight");
+		System.out.println(obj);
+
+	}
+
+}
