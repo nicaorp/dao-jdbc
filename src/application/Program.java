00554ed15729c2d1859163d87e2d19e195ab65e4
Nicolas Ferreira <nicolasferreira100@gmail.com>
2022-06-28 00:44:29 -0300

    Add classe hero

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Class;
+import model.entities.Hero;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Class obj = new Class(1, "Knight");
-		System.out.println(obj);
+		System.out.println(obj);	 
 
+		Hero hero = new Hero(21, "BOB", "bob", new Date(), 20.0, obj);
+		System.out.println(hero);
+		
 	}
 
 }
---
+++ src/model/entities/Hero.java
@@ -0,0 +1,110 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Hero implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String login;
+	private Date createdDate;
+	private Double champLevel;
+	
+	private Class classId;
+	
+	public Hero() {}
+
+	public Hero(Integer id, String name, String login, Date createdDate, Double champLevel, Class classId) {
+		this.id = id;
+		this.name = name;
+		this.login = login;
+		this.createdDate = createdDate;
+		this.champLevel = champLevel;
+		this.classId = classId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Double getChampLevel() {
+		return champLevel;
+	}
+
+	public void setChampLevel(Double champLevel) {
+		this.champLevel = champLevel;
+	}
+
+	public Class getClassId() {
+		return classId;
+	}
+
+	public void setClassId(Class classId) {
+		this.classId = classId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hero other = (Hero) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Hero [id=" + id + ", name=" + name + ", login=" + login + ", createdDate=" + createdDate
+				+ ", champLevel=" + champLevel + ", classId=" + classId + "]";
+	}
+
+	
+	
+	
+}
